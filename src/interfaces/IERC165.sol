c531ab8af664d3d94437b23bb8f8b311a5c2f50b
Bradley Woolf <bradleymwoolf@gmail.com>
2021-11-22 12:04:10 -0800

    interfaces init

---
+++ src/interfaces/IERC165.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.3.2 (utils/introspection/IERC165.sol)
+
+pragma solidity ^0.8.0;
+
+
+interface IERC165 {
+
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
